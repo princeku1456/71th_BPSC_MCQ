f0105f659e9f8fe68fc7815799032c04f592ff6f
princeku1456 <princeiitk9708@gmail.com>
2025-08-02 09:37:48 +0530

    folder

--- config.js
+++ config.js
@@ -10,38 +10,91 @@ const subjectsConfig = {
             ancient: {
                 name: 'Current Affairs',
                 description: 'Explore questions from Bihar and India CA',
-                chapters: [
-                    { id: 'chapter1_1', name: 'Lecture 1', file: './CA/Lec_1.html' },
-                    { id: 'chapter1_2', name: 'Lecture 2', file: './CA/Lec_2.html' },
-                    { id: 'chapter1_3', name: 'Lecture 3', file: './CA/Lec_3.html' },
-                    { id: 'chapter1_4', name: 'Lecture 4', file: './CA/Lec_4.html' },
-                    { id: 'chapter2_1', name: 'Lecture 5', file: 'Lec_5.html' },
-                    { id: 'chapter2_2', name: 'Lecture 6', file: 'Lec_6.html' },
-                    { id: 'chapter2_3', name: 'Lecture 7', file: 'Lec_7.html' },
-                    { id: 'chapter2_3', name: 'Lecture 8', file: 'Lec_8.html' }
+                chapters: [{
+                        id: 'chapter1_1',
+                        name: 'Lecture 1',
+                        file: 'CA/Lec_1.html'
+                    },
+                    {
+                        id: 'chapter1_2',
+                        name: 'Lecture 2',
+                        file: 'CA/Lec_2.html'
+                    },
+                    {
+                        id: 'chapter1_3',
+                        name: 'Lecture 3',
+                        file: 'CA/Lec_3.html'
+                    },
+                    {
+                        id: 'chapter1_4',
+                        name: 'Lecture 4',
+                        file: 'CA/Lec_4.html'
+                    },
+                    {
+                        id: 'chapter2_1',
+                        name: 'Lecture 5',
+                        file: 'Lec_5.html'
+                    },
+                    {
+                        id: 'chapter2_2',
+                        name: 'Lecture 6',
+                        file: 'Lec_6.html'
+                    },
+                    {
+                        id: 'chapter2_3',
+                        name: 'Lecture 7',
+                        file: 'Lec_7.html'
+                    },
+                    {
+                        id: 'chapter2_3',
+                        name: 'Lecture 8',
+                        file: 'Lec_8.html'
+                    }
                 ]
             },
             medieval: {
                 name: 'Medieval History',
                 description: 'Test your knowledge of medieval periods and kingdoms',
-                chapters: [
-                    { id: 'chapter1', name: 'Chapter 1: Delhi Sultanate', file: 'medieval_1.html' },
-                    { id: 'chapter2', name: 'Chapter 2: Mughal Empire', file: 'medieval_2.html' },
-                    { id: 'chapter3', name: 'Chapter 3: Regional Kingdoms', file: 'medieval_3.html' }
+                chapters: [{
+                        id: 'chapter1',
+                        name: 'Chapter 1: Delhi Sultanate',
+                        file: 'medieval_1.html'
+                    },
+                    {
+                        id: 'chapter2',
+                        name: 'Chapter 2: Mughal Empire',
+                        file: 'medieval_2.html'
+                    },
+                    {
+                        id: 'chapter3',
+                        name: 'Chapter 3: Regional Kingdoms',
+                        file: 'medieval_3.html'
+                    }
                 ]
             },
             modern: {
                 name: 'Modern History',
                 description: 'Practice questions from modern era and recent history',
-                chapters: [
-                    { id: 'chapter1', name: 'Chapter 1: British Colonial Rule', file: 'modern_1.html' },
-                    { id: 'chapter2', name: 'Chapter 2: Freedom Struggle', file: 'modern_2.html' },
-                    { id: 'chapter3', name: 'Chapter 3: Post Independence', file: 'modern_3.html' }
+                chapters: [{
+                        id: 'chapter1',
+                        name: 'Chapter 1: British Colonial Rule',
+                        file: 'modern_1.html'
+                    },
+                    {
+                        id: 'chapter2',
+                        name: 'Chapter 2: Freedom Struggle',
+                        file: 'modern_2.html'
+                    },
+                    {
+                        id: 'chapter3',
+                        name: 'Chapter 3: Post Independence',
+                        file: 'modern_3.html'
+                    }
                 ]
             }
         }
     }
-    
+
     // // GEOGRAPHY SUBJECT
     // geography: {
     //     name: 'Geography',
@@ -67,7 +120,7 @@ const subjectsConfig = {
     //         }
     //     }
     // },
-    
+
     // // SCIENCE SUBJECT
     // science: {
     //     name: 'Science',
